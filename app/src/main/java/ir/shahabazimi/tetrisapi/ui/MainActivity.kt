1e56387d725c9dbf502e7c4209acb2d4bf9e97bd
shahab <azimishahab@gmail.com>
2022-01-03 00:22:09 +0330

    appbar added, footer loading added

--- app/src/main/java/ir/shahabazimi/tetrisapi/ui/MainActivity.kt
+++ app/src/main/java/ir/shahabazimi/tetrisapi/ui/MainActivity.kt
@@ -3,10 +3,17 @@ package ir.shahabazimi.tetrisapi.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ir.shahabazimi.tetrisapi.R
+import ir.shahabazimi.tetrisapi.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var b:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+        b.topAppBar.title = getString(R.string.app_name)
+
     }
 }
